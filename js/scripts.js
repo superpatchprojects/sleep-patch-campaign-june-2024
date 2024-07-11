ee6e69f0df0341ba4e5d4681d330aab55c9ca777
lingataira <airal@superpatch.com>
2024-07-11 14:11:36 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
-(function ($) {
-	$(document).ready(function () {
+(async function ($, ShopifyBuy) {
+	const shopifyClient = ShopifyBuy.buildClient({
+		domain: 'a0c1e3-25.myshopify.com',
+		storefrontAccessToken: '87f20013717bc33265c0ab86ead28dc0'
+	});
 
+	const productId = "gid://shopify/Product/" + document.body.dataset.product;
+	const variantId = "gid://shopify/ProductVariant/" + document.body.dataset.variant;
+	const max_qty_available = parseInt(document.body.dataset.maxQuantity);
+
+	const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+	const checkoutButtons = document.querySelectorAll('[data-action="checkout"]');
+	const plusButtons = document.querySelectorAll('.plus-btn');
+	const minusButtons = document.querySelectorAll('.minus-btn');
+	const inputFields = document.querySelectorAll('.quantity');
+	const quantitySelect = document.querySelector('.quantity-select');
+	const totalPriceElement = document.getElementById('totalPrice');
+	const pricePerItem = 60.00;
+
+	var checkout = await shopifyClient.checkout.create();
+
+	$(document).ready(function () {
 		// carousels
 		$('.reviews').owlCarousel({
 			loop: true,
@@ -19,7 +38,6 @@
 			}
 		});
 
-
 		$('.advisory-board').owlCarousel({
 			loop: true,
 			margin: 10,
@@ -36,7 +54,6 @@
 				}
 			}
 		});
-
 
 		$('.rem-percentage').owlCarousel({
 			loop: true,
@@ -71,86 +88,125 @@
 				}
 			}
 		});
+	});
 
-		// how it works video
-		document.getElementById('playButton').addEventListener('click', function () {
-			var container = document.querySelector('.video');
-			var videoHtml = `
-				<div class="col-md-8 offset-md-2 mb-4">
-					<div class="ratio ratio-16x9">
-						<iframe src="https://www.youtube.com/embed/d-nrcP5avSs?autoplay=1&rel=0" title="How It Works" allowfullscreen></iframe>
-					</div>
+	// how it works video
+	document.getElementById('playButton').addEventListener('click', function () {
+		var container = document.querySelector('.video');
+		var videoHtml = `
+			<div class="col-md-8 offset-md-2 mb-4">
+				<div class="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/d-nrcP5avSs?autoplay=1&rel=0" title="How It Works" allowfullscreen></iframe>
 				</div>
-			`;
+			</div>
+		`;
 
-			container.innerHTML = videoHtml;
+		container.innerHTML = videoHtml;
 
-			var videoSection = document.querySelector('.video-section');
-			videoSection.style.padding = '0';
+		var videoSection = document.querySelector('.video-section');
+		videoSection.style.padding = '0';
+	});
+
+
+	// add to cart and cart modal
+	inputFields.forEach(inp => inp.max = max_qty_available);
+
+	addToCartButtons.forEach(button => {
+		button.addEventListener('click', async function () {
+
+			const spinner = this.nextElementSibling;
+
+			setTimeout(() => {
+				spinner.style.display = 'block';
+			}, 100);
+
+			let quantity = parseInt(button.closest(".row").querySelector("input").value);
+			checkout = await shopifyClient.checkout.addLineItems(checkout.id, { variantId, quantity });
+			let new_qty_available = max_qty_available - checkout.lineItems[0].quantity;
+			inputFields.forEach(inp => { inp.max = new_qty_available; inp.value = Math.min(1, new_qty_available); });
+			quantitySelect.value = checkout.lineItems[0].quantity;
+			updateTotalPrice(max_qty_available - new_qty_available);
+			document.getElementById('cartModalOverlay').style.display = max_qty_available - new_qty_available == 0 ? '' : 'block';
+			quantitySelect.closest(".row").querySelector('button').dataset.quantity = max_qty_available - new_qty_available;
+
+			spinner.style.display = 'none';
 		});
+	});
 
+	quantitySelect.addEventListener('change', async function () {
+		const spinner = this.nextElementSibling;
 
-		// add to cart and cart modal
-		document.querySelectorAll('.add-to-cart-btn').forEach(button => {
-			button.addEventListener('click', function () {
-				document.getElementById('cartModalOverlay').style.display = 'block';
-			});
-		});
+		setTimeout(() => {
+			spinner.style.display = 'block';
+		}, 100);
 
-		document.querySelector('.close-btn').addEventListener('click', function () {
-			document.getElementById('cartModalOverlay').style.display = 'none';
-		});
+		let id = checkout.lineItems[0].id;
+		let quantity = parseInt(this.value);
 
-		const plusButtons = document.querySelectorAll('.plus-btn');
-		const minusButtons = document.querySelectorAll('.minus-btn');
-		const inputFields = document.querySelectorAll('.quantity');
-		const quantitySelect = document.querySelector('.quantity-select');
-		const totalPriceElement = document.getElementById('totalPrice');
-		const pricePerItem = 60.00;
-
-		function updateQuantities(value) {
-			inputFields.forEach(input => {
-				input.value = value;
-			});
-			quantitySelect.value = value;
-			updateTotalPrice(value);
-		}
-
-		function updateTotalPrice(quantity) {
-			const totalPrice = pricePerItem * quantity;
-			totalPriceElement.textContent = `$${totalPrice.toFixed(2)}`;
-		}
-
-		plusButtons.forEach(button => {
-			button.addEventListener('click', function () {
-				let newValue = parseInt(inputFields[0].value) + 1;
-				if (newValue <= 10) {
-					updateQuantities(newValue);
-				}
-			});
-		});
-
-		minusButtons.forEach(button => {
-			button.addEventListener('click', function () {
-				let currentValue = parseInt(inputFields[0].value);
-				if (currentValue > 1) {
-					let newValue = currentValue - 1;
-					updateQuantities(newValue);
-				}
-			});
-		});
-
-		quantitySelect.addEventListener('change', function () {
-			let newValue = parseInt(this.value);
-			updateQuantities(newValue);
-		});
+		checkout = await shopifyClient.checkout.updateLineItems(checkout.id, { id, quantity });
+		let new_qty_available = max_qty_available - (checkout.lineItems[0] ? checkout.lineItems[0].quantity : 0);
+		inputFields.forEach(inp => { inp.max = new_qty_available; inp.value = Math.min(1, new_qty_available); });
+		updateTotalPrice(max_qty_available - new_qty_available);
+		document.getElementById('cartModalOverlay').style.display = max_qty_available - new_qty_available == 0 ? '' : 'block';
+		this.closest(".row").querySelector('button').dataset.quantity = max_qty_available - new_qty_available;
 		
+		spinner.style.display = 'none';
 	});
-})(jQuery);
 
-(function (Client) {
-	window.client = Client.buildClient({
-		domain: 'a0c1e3-25.myshopify.com',
-		storefrontAccessToken: '87f20013717bc33265c0ab86ead28dc0'
+	checkoutButtons.forEach(ckbt => {
+		ckbt.addEventListener('click', async function (e) {
+			e.preventDefault();
+			const spinner = this.nextElementSibling;
+
+			if (this.dataset.quantity != 0) {
+				setTimeout(() => {
+					spinner.style.display = 'block';
+				}, 100);
+
+				if (!checkout.lineItems[0]) {
+					checkout = await shopifyClient.checkout.addLineItems(checkout.id, { variantId, quantity: parseInt(this.dataset.quantity) });
+				}
+				spinner.style.display = 'none';
+				location = checkout.webUrl;
+			}
+		});
+	})
+
+	document.querySelector('.close-btn').addEventListener('click', function () {
+		document.getElementById('cartModalOverlay').style.display = 'none';
 	});
-})(ShopifyBuy)
+
+	function updateQuantities(value) {
+		inputFields.forEach(input => {
+			input.value = value;
+		});
+	}
+
+	function updateTotalPrice(quantity) {
+		const totalPrice = pricePerItem * quantity;
+		totalPriceElement.textContent = `$${totalPrice.toFixed(2)}`;
+	}
+
+	inputFields.forEach(qtyfield => {
+		qtyfield.addEventListener("change", function () {
+			updateQuantities(qtyfield.value);
+		})
+	})
+
+	plusButtons.forEach(button => {
+		button.addEventListener('click', function () {
+			let newValue = parseInt(inputFields[0].value) + 1;
+			updateQuantities(Math.min(newValue, inputFields[0].max));
+		});
+	});
+
+	minusButtons.forEach(button => {
+		button.addEventListener('click', function () {
+			let currentValue = parseInt(inputFields[0].value);
+			if (currentValue > 1) {
+				let newValue = currentValue - 1;
+				updateQuantities(Math.min(newValue, inputFields[0].max));
+			}
+		});
+	});
+})(jQuery, ShopifyBuy);
