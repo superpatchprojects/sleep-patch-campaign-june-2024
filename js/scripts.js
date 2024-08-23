717f24c8569c280fb0e5ce48b956e36adc2388a2
superpatchsites <himanshu.r@superpatch.com>
2024-08-23 11:10:59 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -87,10 +87,10 @@ jQuery(document).ready(function () {
 
 (function ($, ShopifyBuy) {
 	const params = new URLSearchParams(location.search);
-	params.entries().forEach(function([k,v]){
+	for (const [key, value] of params.entries()){
 		alert(k);
 		//sessionStorage.setItem(k,v)
-	});
+	};
 	alert("Params found:" + params.size);
 	sessionStorage.setItem("hello","hello");
 	alert(sessionStorage.getItem("hello"));
